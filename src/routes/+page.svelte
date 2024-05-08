40b09e78ebc72c6500a8c03c7bfa87d3e2b6f2d8
polypixeldev <79737178+polypixeldev@users.noreply.github.com>
2024-05-07 21:35:10 -0400

    Basic login using Matrix SDK

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,40 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import * as sdk from 'matrix-js-sdk';
+	import { goto } from '$app/navigation';
+
+	let client = null;
+	let homeserver = '';
+	let username = '';
+	let password = '';
+</script>
+
+<h1>Purple Bubble - Login</h1>
+<label>
+	Homeserver
+	<input id="homeserver-input" type="text" bind:value={homeserver} />
+</label>
+<br />
+<label>
+	Username
+	<input id="username-input" type="text" bind:value={username} />
+</label>
+<br />
+<label>
+	Password
+	<input id="password-input" type="password" bind:value={password} />
+</label>
+<br />
+
+<button
+	on:click={async () => {
+		client = sdk.createClient({
+			baseUrl: homeserver
+		});
+
+		await client.login('m.login.password', { user: username, password });
+
+		console.log('Logged in');
+
+		client.startClient();
+	}}>Login</button
+>
